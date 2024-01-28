9e63fd1a12e6f39aa0a33f3d6a399ad5e5984d10
Ahmed Raed <ahmedraed101@yahoo.com>
2024-01-28 22:19:24 +0300

    adding-tasks-and-draggable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,43 @@
   <div class="container">
     <div>
       <div>Tasks</div>
+      <div class="items-box">
+        <draggable v-model="tasks" tag="ul" group="todo">
+          <template #item="{ element: task }">
+            <li>{{ task }}</li>
+          </template>
+        </draggable>
+      </div>
     </div>
     <div>
       <div>In Progress</div>
+      <div class="items-box">
+        <draggable v-model="inProgress" tag="ul" group="todo">
+          <template #item="{ element: task }">
+            <li>{{ task + ' ⏱' }}</li>
+          </template>
+        </draggable>
+      </div>
     </div>
     <div>
       <div>Done</div>
+      <div class="items-box">
+        <draggable v-model="done" tag="ul" group="todo">
+          <template #item="{ element: task }">
+            <li>{{ task + ' ✅' }}</li>
+          </template>
+        </draggable>
+      </div>
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import draggable from 'vuedraggable'
+const tasks = ref(['Programming', 'Trading', 'UI/UX design', 'FrontEnd', 'BackEnd'])
+const inProgress = ref(['HomeWork'])
+const done = ref(['practicing test'])
+</script>
 <style scoped>
 .container {
   display: flex;
@@ -38,5 +65,21 @@
   border-bottom: 1px solid black;
   margin-bottom: 5px;
 }
-/* update */
+
+.container > div > .items-box {
+  width: 100%;
+  height: calc(100% - 40px);
+  padding: 3px;
+}
+.container > div > .items-box > ul {
+  list-style: none;
+  padding: 0;
+}
+.container > div > .items-box > ul > li {
+  box-sizing: border-box;
+  padding: 0 3px;
+  min-height: 24px;
+  margin-bottom: 3px;
+  background-color: white;
+}
 </style>
